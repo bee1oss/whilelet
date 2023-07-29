906f47768aa8374910639ff1ef6ef3b5950d67e3
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-29 10:55:37 +0300

    variants
    varints write counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    
+    let mut counter = Some(0);
+
+    while let Some(value) = counter {
+        if value == 31 {
+            println!("End!");
+            counter = None;
+        } else {
+            println!("{}", value);
+            counter = Some(value + 1);
+        }
+    }
+
+    /*loop {
+        match counter {
+            None => break,
+            Some(value) => {
+                if value==31 {
+                    println!("End!");
+                    counter=None;
+                }else {
+                    println!("{}",value);
+                    counter = Some(value+1);
+                }
+            }
+        }
+    }*/
 }
