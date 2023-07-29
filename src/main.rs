3493fde63806abee6a7c0dc353bff87a9447dd93
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-29 11:03:28 +0300

    end

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
+    let dat = vec![
+        vec!["Den","11","10","12"],
+        vec!["Kate","8","4","9"],
+        vec!["John","6","5","2"],
+    ];
+
+    for mut student in dat {
+        print!("For {} :",student[0]);
+        while let Some(value)=student.pop() {
+            if let Ok(result)=value.parse::<i32>(){
+                print!(" {} ",result);
+            }else {
+                println!(" ");
+            }
+        }
+    }
+}
+
+fn counter_func(){
     let mut counter = Some(0);
 
     while let Some(value) = counter {
